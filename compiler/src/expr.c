4f6b5330eaecb3c2d76c35580470b586ba7936b0
Adnan Faizi <adnan500@my.yorku.ca>
2024-11-13 21:33:33 -0500

    bug fixed for type mismatches and variable renaming

--- compiler/src/expr.c
+++ compiler/src/expr.c
@@ -1,6 +1,7 @@
 #include "../include/expr.h"
 #include <stdlib.h>
 
+/* Used to create a general expression where its lift and right childs are also expressions */
 struct expr *expr_create(expr_t kind, struct expr *left, struct expr *right) {
   struct expr *e = malloc(sizeof(*e));
   e->kind = kind;
@@ -10,34 +11,37 @@ struct expr *expr_create(expr_t kind, struct expr *left, struct expr *right) {
   return e;
 }
 
-// TODO(Ali)
-// NOTE(Ali): Note sure what this thing is, probably an identifier
+/* Used to create identifier expression */
 struct expr * expr_create_name( const char *n ) { 
-  struct expr* str_literal =  expr_create(EXPR_NAME, NULL, NULL);
-  str_literal->name = n;
-  return str_literal;
+  struct expr* identifier_expr =  expr_create(EXPR_NAME, NULL, NULL);
+  identifier_expr->name = n;
+  return identifier_expr;
 }
 
+/* Used to create integer literal expression */
 struct expr * expr_create_integer_literal( int c ) { 
-  struct expr* int_literal =  expr_create(EXPR_INTEGER_LITERAL, NULL, NULL);
-  int_literal->literal_value = c;
-  return int_literal;
+  struct expr* int_literal_expr =  expr_create(EXPR_INTEGER_LITERAL, NULL, NULL);
+  int_literal_expr->literal_value = c;
+  return int_literal_expr;
 }
 
+/* Used to create boolean literal expression */
 struct expr * expr_create_boolean_literal( int c ) { 
-  struct expr* bool_literal =  expr_create(EXPR_BOOLEAN_LITERAL, NULL, NULL);
-  bool_literal->literal_value = c;
-  return bool_literal;
+  struct expr* bool_literal_expr =  expr_create(EXPR_BOOLEAN_LITERAL, NULL, NULL);
+  bool_literal_expr->literal_value = c;
+  return bool_literal_expr;
 }
 
+/* Used to create character literal expression */
 struct expr * expr_create_char_literal( char c ) { 
-  struct expr* char_literal =  expr_create(EXPR_CHAR_LITERAL, NULL, NULL);
-  char_literal->literal_value = c;
-  return char_literal;
+  struct expr* char_literal_expr =  expr_create(EXPR_CHAR_LITERAL, NULL, NULL);
+  char_literal_expr->literal_value = c;
+  return char_literal_expr;
 }
 
+/* Used to create string literal expression */
 struct expr * expr_create_string_literal( const char *str ) { 
-  struct expr* str_literal =  expr_create(EXPR_CHAR_LITERAL, NULL, NULL);
-  str_literal->string_literal = str;
-  return str_literal;
+  struct expr* str_literal_expr =  expr_create(EXPR_STRING_LITERAL, NULL, NULL);
+  str_literal_expr->string_literal = str;
+  return str_literal_expr;
 }
