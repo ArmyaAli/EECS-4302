d9264a0829ae0c83bbc8602cc1df5dd78d25ec9a
Ali Umar <aumar3@red.eecs.yorku.ca>
2024-11-23 11:36:32 -0500

    cleanup more imports

--- compiler/src/main.c
+++ compiler/src/main.c
@@ -1,9 +1,9 @@
 #define _CRT_SECURE_NO_DEPRECATE
 
-#include "include/messages.h"
-#include "include/pipeline.h"
 #include <stdlib.h>
 #include <stdio.h>
+#include "include/messages.h"
+#include "include/pipeline.h"
 
 int main(int argc, char *argv[]) {
     const char *option = argv[1];
